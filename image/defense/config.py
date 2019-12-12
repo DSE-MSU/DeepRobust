2a1e4d2e4e4f236a09b68d7c75f564e32c34302d
hannxu123 <48928341+hannxu123@users.noreply.github.com>
2019-12-12 17:28:12 -0500

    Update config.py
    Former-commit-id: 2758fc09887c0779648db4640dae9fdffb117ef5
    Former-commit-id: 0f1bf825b5a34a7a39b39b95b69c545e5087b632
    Former-commit-id: f7b24dd681c4729afbe24fe154349e1f22a3a3e8

--- image/defense/config.py
+++ image/defense/config.py
@@ -1,22 +1,32 @@
-from easydict import EasyDict
 import sys
 import os
 import argparse
-import numpy as np
 import torch
+from typing import Tuple, List, Dict
+
 
 def add_path(path):
     if path not in sys.path:
         print('Adding {}'.format(path))
         sys.path.append(path)
 
+
 abs_current_path = os.path.realpath('./')
 root_path = os.path.join('/', *abs_current_path.split(os.path.sep)[:-3])
 lib_dir = os.path.join(root_path, 'lib')
 add_path(lib_dir)
 
-class TrainingConfing(TrainingConfigBase):
+class PieceWiseConstantLrSchedulerMaker(object):
 
+    def __init__(self, milestones:List[int], gamma:float = 0.1):
+        self.milestones = milestones
+        self.gamma = gamma
+
+    def __call__(self, optimizer):
+        return torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=self.milestones, gamma=self.gamma)
+
+
+class TrainingConfing():
     ## model saving path
     lib_dir = lib_dir
 
@@ -24,31 +34,23 @@ class TrainingConfing(TrainingConfigBase):
     num_epochs = 105
     batch_size = 100
     val_interval = 10
-    
+
     ## optimizer
     optimizer = 'SGD'
     learning_rate = 1e-4
-    create_lr_scheduler = PieceWiseConstantLrSchedulerMaker(milestones = [70, 90, 100], gamma = 0.1)
+    create_lr_scheduler = PieceWiseConstantLrSchedulerMaker(milestones=[70, 90, 100], gamma=0.1)
     momentum = 0.2
     weight_decay = 5e-4
-    
+
 config = TrainingConfing()
 
 
 
 parser = argparse.ArgumentParser()
-
-parser.add_argument('--resume', default=None, type=str, metavar='PATH',
-                 help='path to latest checkpoint (default: none)')
-parser.add_argument('-b', '--batch_size', default=256, type=int,
-                 metavar='N', help='mini-batch size')
-parser.add_argument('-d', type=int, default=0, help='Which gpu to use')
-parser.add_argument('-adv_coef', default=1.0, type = float,
-                    help = 'Specify the weight for adversarial loss')
-parser.add_argument('--auto-continue', default=False, action = 'store_true',
-                    help = 'Continue from the latest checkpoint')
+parser.add_argument('--batch_size', default=200, type=int,
+                    metavar='N', help='mini-batch size')
+parser.add_argument('--save_dir', default='', help = 'save your model to this path')
 args = parser.parse_args()
-
 
 if __name__ == '__main__':
     pass
