468ade5ae02563b7201e64a8a27c679b5e42d1a3
hannxu123 <48928341+hannxu123@users.noreply.github.com>
2019-12-12 11:15:23 -0500

    Update CNN.py
    Former-commit-id: e66fff651805c2348a077f6eec1de70c8603c571
    Former-commit-id: 9d51129826dbf252a9037d7481469a7bb2b51976
    Former-commit-id: e0cdd0cfce9b9f254a9795f8efb0e5574110626d

--- image/netmodels/CNN.py
+++ image/netmodels/CNN.py
@@ -9,7 +9,7 @@ import numpy as np
 from PIL import Image
 
 class Net(nn.Module):
-    def __init__(self, in_channel1 = 1, out_channel1 = 20, out_channel2 = 50):
+    def __init__(self, in_channel1 = 1, out_channel1 = 32, out_channel2 = 64):
         super(Net, self).__init__()
 
         ##define two convolutional layers
@@ -17,28 +17,28 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(out_channel1, out_channel2, 5, 1)
 
         ##define two linear layers
-        self.fc1 = nn.Linear(4 * 4 * 50, 500)
-        self.fc2 = nn.Linear(500, 10)
+        self.fc1 = nn.Linear(7 * 7 * out_channel2, 1024)
+        self.fc2 = nn.Linear(1024, 10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4* 4 * 50)
+        x = x.view(-1, 7* 7 * output_channel2)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
-    def get_logits(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4* 4 * 50)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+    #def get_logits(self, x):
+        #x = F.relu(self.conv1(x))
+        #x = F.max_pool2d(x, 2, 2)
+        #x = F.relu(self.conv2(x))
+        #x = F.max_pool2d(x, 2, 2)
+        #x = x.view(-1, 4* 4 * 50)
+        #x = F.relu(self.fc1(x))
+        #x = self.fc2(x)
+        #return x
 
 
 def train(model, device, train_loader, optimizer, epoch):
