a091c5a544834e85a0fb731a64ad5ea7cb962b52
hannxu123 <48928341+hannxu123@users.noreply.github.com>
2019-12-12 14:05:05 -0500

    Update CNN.py

--- image/netmodels/CNN.py
+++ image/netmodels/CNN.py
@@ -14,12 +14,20 @@ class Net(nn.Module):
         
         self.out_channel2 = out_channel2
         
-        ##define two convolutional layers
-        self.conv1 = nn.Conv2d(in_channel1, out_channel1, 5, 1)
-        self.conv2 = nn.Conv2d(out_channel1, out_channel2, 5, 1)
+        ## define two convolutional layers
+        self.conv1 = nn.Conv2d(in_channels= in_channel1,
+                               out_channels= out_channel1,
+                               kernel_size= 5,
+                               stride= 1,
+                               padding = (2,2))
+        self.conv2 = nn.Conv2d(in_channels= out_channel1,
+                               out_channels= out_channel2,
+                               kernel_size= 5,
+                               stride= 1,
+                               padding = (2,2))
 
-        ##define two linear layers
-        self.fc1 = nn.Linear(4 * 4 * out_channel2, 1024)
+        ## define two linear layers
+        self.fc1 = nn.Linear(7 * 7 * out_channel2, 1024)
         self.fc2 = nn.Linear(1024, 10)
 
     def forward(self, x):
@@ -27,7 +35,7 @@ class Net(nn.Module):
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4* 4 * self.out_channel2)
+        x = x.view(-1, 7* 7 * self.out_channel2)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
