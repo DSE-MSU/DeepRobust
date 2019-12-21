9a698bd4b6126768553c21211d1271c2484b1d28
Yaxin Li <49900442+I-am-Bot@users.noreply.github.com>
2019-12-21 18:12:02 -0500

    update nattack
    Former-commit-id: d85c305e7b72d60a83acf231e1688a0a048d4d93
    Former-commit-id: d45a2257707eedbf4fb73cae4fe789feca101968
    Former-commit-id: 70d896c44c59c9013740327648712b843af2e9ab

--- tutorials/test_nattack.py
+++ tutorials/test_nattack.py
@@ -11,9 +11,14 @@ from DeepRobust.image.netmodels.CNN import Net
 model = Net()
 model.load_state_dict(torch.load("DeepRobust/image/save_models/mnist_pgdtraining.pt", map_location = torch.device('cpu')))
 model.eval()
+print("----------model_parameters-----------")
 
+for names,parameters in model.named_parameters():
+    print(names,',', parameters.type())
+print("-------------------------------------")
 data_loader = torch.utils.data.DataLoader(
               datasets.MNIST('DeepRobust/image/data', train = True,
+              download = True,
               transform = transforms.Compose([transforms.ToTensor()])),
               batch_size = 1,
               shuffle = True)
