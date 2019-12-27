b2ef6868291c0d71f0baec77ff083f4462c49e8e
ChandlerBang <baocoolwei@qq.com>
2019-12-27 17:35:25 -0500

    add graph attack fgsm
    Former-commit-id: 35de0ae58137ae0def757164e27c82ad257f9327
    Former-commit-id: c2e690e30545747d72d0911b2494d24c5079b0e3
    Former-commit-id: a453869b1add6e21996369833f21c211235bfb0d

---
+++ graph/defense/gcn.py
@@ -0,0 +1,88 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import torch
+import torch.optim as optim
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+from DeepRobust.graph import utils
+
+class GraphConvolution(Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, with_bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        if with_bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj):
+        support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
+
+
+class GCN(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout=0.5, with_relu=True, with_bias=True):
+        super(GCN, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid, with_bias=with_bias)
+        self.gc2 = GraphConvolution(nhid, nclass, with_bias=with_bias)
+        self.dropout = dropout
+        self.with_relu = with_relu
+
+    def forward(self, x, adj):
+        if self.with_relu:
+            x = F.relu(self.gc1(x, adj))
+        else:
+            x = self.gc1(x, adj)
+
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        return F.log_softmax(x, dim=1)
+
+    def initialize(self):
+        self.gc1.reset_parameters()
+        self.gc2.reset_parameters()
+
+    def fit(self, x, adj, labels, idx_train, train_iters=200):
+
+        self.initialize()
+        self.train()
+        try:
+            adj.layout
+            adj_norm = utils.normalize_adj_tensor(adj, sparse=True)
+        except:
+            adj_norm = utils.normalize_adj_tensor(adj)
+
+        optimizer = optim.Adam(self.parameters(), lr=0.01, weight_decay=5e-4)
+
+        print('=== training surrogate model ===')
+        for i in range(train_iters):
+            optimizer.zero_grad()
+            output = self.forward(x, adj_norm)
+            loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+            loss_train.backward()
+            optimizer.step()
+
