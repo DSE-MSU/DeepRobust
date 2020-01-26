6bf9f20d004679741f67e4de7cbf394cacbb91b8
Yaxin Li <liyaxin1@dev-intel16-k80.i>
2020-01-25 20:04:28 -0500

    test defense & fix bugs
    Former-commit-id: c2da39d789bfbc31a9571c4951766411c2159b0f
    Former-commit-id: d992b1cec8ec23b33964267cb325310b733c7c4d
    Former-commit-id: 78816ca1cf923db6e7b90c9ea9aafe1d40ee7cc4

--- tutorials/test_trade.py
+++ tutorials/test_trade.py
@@ -22,30 +22,5 @@ model = Net()
 defense = TRADES(model,'cuda')
 defense.generate(train_loader, test_loader)
 
-xx = datasets.MNIST('../data').data[3333]
-xx = xx.unsqueeze_(0).float()/255
-xx = xx.unsqueeze_(0).float()
 
-## Set Targetå
-yy = datasets.MNIST('DeepRobust/image/data', download=True).targets[3333]
-yy = yy.unsqueeze_(0).float()
-
-predict0 = defensemodel(xx)
-predict0= predict0.argmax(dim=1, keepdim=True)
-
-adversary = PGD(model)
-AdvExArray = adversary.generate(xx,yy, epsilon = 0.3)
-
-# predict1 = model(AdvExArray)
-# predict1= predict1.argmax(dim=1, keepdim=True)
-
-AdvExArray = AdvExArray.cpu()
-predict2 = defensemodel(AdvExArray)
-predict2= predict2.argmax(dim=1, keepdim=True)
-
-print(predict0)
-# print(predict1)
-print(predict2)
-
-AdvExArray = AdvExArray.cpu().detach().numpy()
 
