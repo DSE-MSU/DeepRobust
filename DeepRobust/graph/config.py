b70f0163d52caee667370c06fb9e59f3af961fd4
ChandlerBang <baocoolwei@qq.com>
2020-01-27 10:51:35 -0500

    change username and minor changes
    Former-commit-id: 53946b93b4c840b8929f133c871900c81b034c37
    Former-commit-id: 75e2b9bf1fbf5820358d03ce81fe340080ab1bda
    Former-commit-id: c1662e745fe864b96731802d616a0fbe7348d251

---
+++ DeepRobust/graph/config.py
@@ -0,0 +1,17 @@
+attack_params = {
+    "FGSM_MNIST": {
+    'epsilon': 0.2,
+    'clip_max': None,
+    'clip_min': None
+    }
+}
+
+
+rl_agent_params = {
+    "FGSM_MNIST": {
+    'epsilon': 0.2,
+    'clip_max': None,
+    'clip_min': None
+    }
+}
+
