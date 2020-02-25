bd6b94a11b33fb495768db7c20946b26d71333de
ChandlerBang <baocoolwei@qq.com>
2020-02-25 18:15:19 -0500

    modify deeprobust/__init__.py

--- deeprobust/__init__.py
+++ deeprobust/__init__.py
@@ -1,8 +1,4 @@
-import logging
-from deeprobust import image
-from deeprobust import graph
-
-__all__ = ['image', 'graph']
-logging.basicConfig(filename = "test.log", filemode = "w")
-logging.info('import image')
-logging.info('import graph')
+# from deeprobust import image
+# from deeprobust import graph
+#
+# __all__ = ['image', 'graph']
