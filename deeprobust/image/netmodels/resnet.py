5de50ef027b092b779b654763cdd15d443891cf6
hannxu123 <48928341+hannxu123@users.noreply.github.com>
2020-04-15 00:57:12 -0400

    Update resnet.py

--- deeprobust/image/netmodels/resnet.py
+++ deeprobust/image/netmodels/resnet.py
@@ -144,7 +144,8 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            #test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            loss = F.cross_entropy(output, target)
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
