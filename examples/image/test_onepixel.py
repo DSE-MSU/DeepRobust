bcc0851d309084c4f368553c0e582b5ade5bf2b2
Yaxin Li <liyaxin1@dev-intel16-k80.i>
2020-10-24 22:01:06 -0400

    <add> example code for one pixel attack

--- examples/image/test_onepixel.py
+++ examples/image/test_onepixel.py
@@ -1,11 +1,30 @@
+import numpy as np
 import torch
-from torchvision import datasets, transforms
+import torch.nn as nn
+import torch.nn.functional as F #233
+import torch.optim as optim
+from torchvision import datasets,models,transforms
+from PIL import Image
+import argparse
 
 from deeprobust.image.attack.onepixel import Onepixel
-import deeprobust.image.netmodels.resnet as resnet
-import deeprobust.image.netmodels.CNN as CNN
+from deeprobust.image.netmodels import resnet
 from deeprobust.image.config import attack_params
-import matplotlib.pyplot as plt
+from deeprobust.image.utils import download_model
+
+def parameter_parser():
+    parser = argparse.ArgumentParser(description = "Run attack algorithms.")
+
+    parser.add_argument("--destination",
+                        default = './trained_models/',
+                        help = "choose destination to load the pretrained models.")
+
+    parser.add_argument("--filename",
+                        default = "MNIST_CNN_epoch_20.pt")
+
+    return parser.parse_args()
+
+args = parameter_parser() # read argument and creat an argparse object
 
 model = resnet.ResNet18().to('cuda')
 print("Load network")
@@ -22,15 +41,40 @@ test_loader  = torch.utils.data.DataLoader(
                 transform = transform_val),
                 batch_size = 1, shuffle=True) #, **kwargs)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+classes = np.array(('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'))
 
 xx, yy = next(iter(test_loader))
 xx = xx.to('cuda').float()
 
-onepixel_params = {
-    'pixels':1
-}
-print(xx.size())
-attack = Onepixel(model,'cuda')
-success, rate = attack.generate(image = xx, label = yy, **onepixel_params)
-print(success, rate)
+"""
+Generate adversarial examples
+"""
+
+F1 = Onepixel(model, device = "cuda")       ### or cuda
+AdvExArray = F1.generate(xx, yy)
+
+predict0 = model(xx)
+predict0= predict0.argmax(dim=1, keepdim=True)
+
+predict1 = model(AdvExArray)
+predict1= predict1.argmax(dim=1, keepdim=True)
+
+print("original prediction:")
+print(predict0)
+
+print("attack prediction:")
+print(predict1)
+
+xx = xx.cpu().detach().numpy()
+AdvExArray = AdvExArray.cpu().detach().numpy()
+
+import matplotlib.pyplot as plt
+xx = xx[0].transpose(1, 2, 0)
+AdvExArray = AdvExArray[0].transpose(1, 2, 0)
+
+plt.imshow(xx, vmin=0, vmax=255)
+plt.savefig('./adversary_examples/cifar10_advexample_ori.png')
+
+plt.imshow(AdvExArray, vmin=0, vmax=255)
+plt.savefig('./adversary_examples/cifar10_advexample_onepixel_adv.png')
