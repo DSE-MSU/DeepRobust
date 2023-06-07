ff5148ab4814892a9386ee3461ce5cb97476958a
wentao-repo <wangwentao91@gmail.com>
2023-06-06 23:10:57 -0400

    add new method and new dataset

--- deeprobust/llm_detection/main.py
+++ deeprobust/llm_detection/main.py
@@ -3,10 +3,12 @@ import argparse
 parser = argparse.ArgumentParser(description='text test')
 
 parser.add_argument('--seed', type=int, default=0)
-parser.add_argument('--method', default='gpt2_detector', type=str, help='name of detection method')
+parser.add_argument('--method', default='GLTR', type=str, help='name of detection method')
 parser.add_argument('--target_text', default='', type=str, help='text for detection')
 parser.add_argument('--customized_dataset', type=str, default='', help='whether using customized dataset for detection')
 parser.add_argument('--dataset', default='xsum', type=str, help='dataset used for model training and evaluation')
+parser.add_argument('--key_name', type=str, default='', help='key name of the dataset from huggingface')
+parser.add_argument('--synthetic_dataset_name', type=str, default='', help='name of the customized dataset')
 parser.add_argument('--load_hf_dataset', type=str, default='', help='whether loading a dataset from huggingface')
 parser.add_argument('--n_samples', type=int, default=500)
 parser.add_argument('--batch_size', type=int, default=50)
@@ -14,7 +16,7 @@ parser.add_argument('--device', default='cuda', type=str, help='which device use
 parser.add_argument('--base_model_name', type=str, default='gpt2-medium', help='model to generate texts for detection')
 
 parser.add_argument('--openai_model', type=str, default=None)
-parser.add_argument('--openai_key', type=str, default='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')
+parser.add_argument('--openai_key', type=str, default='')
 parser.add_argument('--cache_dir', type=str, default='./cache_dir', help='path of the folder to save cache')
 parser.add_argument('--output_to_log', default='', help='whether saving logs into a file')
 
@@ -40,6 +42,7 @@ import transformers
 from utils import *
 from detect_gpt import *
 from gpt2_detector import *
+from gltr import *
 
 
 def main():
@@ -52,6 +55,7 @@ def main():
     torch.cuda.manual_seed_all(args.seed)
     # cudnn.deterministic = True
 
+    print(f'Use DATASET {args.method} to detect', flush=True)
     print(f'Seed number is {args.seed}', flush=True)
 
     device = args.device
@@ -70,7 +74,10 @@ def main():
         if not os.path.exists(args.cache_dir):
             os.makedirs(args.cache_dir)
 
-        data_path = f'./detection_results/{args.dataset}/{base_model_name}'
+        if args.synthetic_dataset_name:
+            data_path = f'./detection_results/{args.dataset}_{args.synthetic_dataset_name}/{base_model_name}'
+        else:
+            data_path = f'./detection_results/{args.dataset}/{base_model_name}'
         results_path = os.path.join(data_path, args.method)
         if not os.path.exists(results_path):
             os.makedirs(results_path)
@@ -87,49 +94,54 @@ def main():
             with open(os.path.join(data_path, 'raw_data.json')) as f:
                 final_data = json.load(f)
 
+            assert len(final_data['original']) >= args.n_samples, "The current dataset does not have enough data, please generate a new dataset or decrease the number of samples"
+
             base_model = None
         else:
             cur_dataset = data_preprocess(args.dataset, args.cache_dir) 
             if args.customized_dataset:
-                cur_dataset.generate_data_customized(args.key_name, args.load_hf_dataset)
+                cur_dataset.generate_data_customized(args.key_name, args.synthetic_dataset_name, args.load_hf_dataset)
             else:
                 cur_dataset.generate_data_predefined()
 
-            preproc_tokenizer = transformers.AutoTokenizer.from_pretrained('t5-small', model_max_length=512, cache_dir=args.cache_dir)
-            if args.dataset in ['wmt16_en', 'wmt16_de']:
-                mask_tokenizer = transformers.AutoTokenizer.from_pretrained(args.mask_filling_model_name, model_max_length=512, cache_dir=args.cache_dir)
-                preproc_tokenizer = mask_tokenizer
+            if cur_dataset.data is not None:
+                preproc_tokenizer = transformers.AutoTokenizer.from_pretrained('t5-small', model_max_length=512, cache_dir=args.cache_dir)
+                if args.dataset in ['wmt16_en', 'wmt16_de']:
+                    mask_tokenizer = transformers.AutoTokenizer.from_pretrained(args.mask_filling_model_name, model_max_length=512, cache_dir=args.cache_dir)
+                    preproc_tokenizer = mask_tokenizer
 
-            # keep only examples with <= 512 tokens according to mask_tokenizer
-            # this step has the extra effect of removing examples with low-quality/garbage content
-            data = cur_dataset.data[:5000]
-            tokenized_data = preproc_tokenizer(data)
-            data = [x for x, y in zip(data, tokenized_data["input_ids"]) if len(y) <= 512]
+                # keep only examples with <= 512 tokens according to mask_tokenizer
+                # this step has the extra effect of removing examples with low-quality/garbage content
+                data = cur_dataset.data[:5000]
+                tokenized_data = preproc_tokenizer(data)
+                data = [x for x, y in zip(data, tokenized_data["input_ids"]) if len(y) <= 512]
 
-            # print stats about remainining data
-            print(f"Total number of samples: {len(data)}", flush=True)
-            print(f"Average number of words: {np.mean([len(x.split()) for x in data])}", flush=True)
+                # print stats about remainining data
+                print(f"Total number of samples: {len(data)}", flush=True)
+                print(f"Average number of words: {np.mean([len(x.split()) for x in data])}", flush=True)
 
-            # define generative model which generates texts
-            base_model, base_tokenizer = load_base_model_and_tokenizer(args.base_model_name, args.openai_model, args.dataset, args.cache_dir)
+                # define generative model which generates texts
+                base_model, base_tokenizer = load_base_model_and_tokenizer(args.base_model_name, args.openai_model, args.dataset, args.cache_dir)
 
-            cur_model = model_collector(base_model=base_model, base_tokenizer=base_tokenizer, device=device, openai_model=args.openai_model)
-            cur_model.load_base_model()
+                cur_model = model_collector(base_model=base_model, base_tokenizer=base_tokenizer, device=device, openai_model=args.openai_model, openai_key=args.openai_key)
+                cur_model.load_base_model()
 
-            final_data = generate_samples(data[:args.n_samples], cur_dataset, cur_model, args.dataset)
+                final_data = generate_samples(data[:args.n_samples], cur_dataset, cur_model, args.dataset)
+            else:
+                final_data = cur_dataset.final_data
 
             # write the data to a json file in the save folder
             with open(os.path.join(data_path, 'raw_data.json'), 'w') as f:
                 print(f"Writing raw data to {os.path.join(data_path, 'raw_data.json')}")
                 json.dump(final_data, f)
 
+            del cur_model
+
         if base_model is None:
             base_model, base_tokenizer = load_base_model_and_tokenizer(args.base_model_name, args.openai_model, args.dataset, args.cache_dir)
 
         if args.openai_model is not None:
-            import openai
             assert args.openai_key is not None, "Must provide OpenAI API key as --openai_key"
-            openai.api_key = args.openai_key
         else:
             base_model = base_model.to(device)
 
@@ -149,10 +161,10 @@ def main():
 
             mask_tokenizer = transformers.AutoTokenizer.from_pretrained(args.mask_filling_model_name, model_max_length=n_positions, cache_dir=args.cache_dir)
 
-            cur_models = model_collector(base_model=base_model, base_tokenizer=base_tokenizer, mask_model=mask_model, mask_tokenizer=mask_tokenizer, device=device, openai_model=args.openai_model)
-            cur_models.load_base_model()            
+            cur_models = model_collector(base_model=base_model, base_tokenizer=base_tokenizer, mask_model=mask_model, mask_tokenizer=mask_tokenizer, device=device, openai_model=args.openai_model, openai_key=args.openai_key)
+            cur_models.load_base_model()
 
-            detect_method = DetectGPT()
+            detect_method = DetectGPT(args.n_samples)
             detect_method.main_func(final_data, cur_models, results_path, args.mask_filling_model_name, args.batch_size)
         elif args.method == 'gpt2_detector':
             if args.pretrained_model == 'yes':
@@ -183,11 +195,19 @@ def main():
             else:
                 pretrained_model = False
 
-            cur_models = model_collector(base_model=base_model, base_tokenizer=base_tokenizer, classification_model=classification_model, classification_tokenizer=classification_tokenizer, device=device, openai_model=args.openai_model)
+
+            cur_models = model_collector(base_model=base_model, base_tokenizer=base_tokenizer, classification_model=classification_model, classification_tokenizer=classification_tokenizer, device=device, openai_model=args.openai_model, openai_key=args.openai_key)
             cur_models.load_classfication_model()
 
-            detect_method = GPT2_detector(pretrained_model)
+            detect_method = GPT2_detector(pretrained_model, args.n_samples)
             detect_method.main_func(final_data, cur_models, results_path, args.batch_size)
+        elif args.method == 'GLTR':
+            cur_models = model_collector(base_model=base_model, base_tokenizer=base_tokenizer, device=device, openai_model=args.openai_model, openai_key=args.openai_key)
+            cur_models.load_base_model()
+
+            detect_method = GLTR(args.n_samples)
+            detect_method.main_func(final_data, cur_models, results_path, args.batch_size)
+
 
 
 
